1618621e3afafa16cf874e3f35a38be3a009c7d7
Krystian Kaźmierczak <git@kazmierczak.one>
2022-01-29 06:00:06 +0100

    delete unnecesary files change export & files path

---
+++ src/core/App/index.js
@@ -0,0 +1,13 @@
+function App() {
+    return (
+      <div>
+        <h1>Test</h1>
+        <h2>Test</h2>
+        <h3>Test</h3>
+        <p> lorem ipsum dolor sit amet, consectetur adip</p>
+      </div>
+    );
+  }
+  
+  export default App;
+  
